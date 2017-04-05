b1289aeadd40600b66a5d5ca0181b1f93049b1d6
leo.zhang <leo.zhang@pkfare.com>
2017-04-05 19:35:36 +0800

    日志logback 简单配置

--- framework/src/main/java/com/mocha/service/UserService.java
+++ framework/src/main/java/com/mocha/service/UserService.java
@@ -2,6 +2,8 @@ package com.mocha.service;
 
 import com.mocha.bean.User;
 import com.mocha.dao.UserDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +14,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     @Autowired
     private UserDao dao;
 
 
     public User getByName(String name) {
+        logger.info("info");
+        logger.debug("debug");
+        logger.error("error");
+        logger.getName();
         return dao.select(name);
     }
 
