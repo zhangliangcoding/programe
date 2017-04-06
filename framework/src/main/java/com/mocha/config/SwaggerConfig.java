b07980dc2ed7e4dfa2fe5f50209d101e2a988918
leo.zhang <leo.zhang@pkfare.com>
2017-04-06 20:03:34 +0800

    swagger 集成

--- framework/src/main/java/com/mocha/config/SwaggerConfig.java
+++ framework/src/main/java/com/mocha/config/SwaggerConfig.java
@@ -19,42 +19,42 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 
 
-    @Bean
-    public Docket addUserDocket() {
-        Docket docket = new Docket(DocumentationType.SWAGGER_2);
-        ApiInfo apiInfo = new ApiInfo(
-                "Spring boot swagger Document API",
-                "API Document管理",
-                "V1.0.0",
-                "",
-                "1239917467@qq.com",
-                "",
-                "");
-        docket.apiInfo(apiInfo).select()
-                .apis(RequestHandlerSelectors.basePackage("com.mocha"))
-                .paths(PathSelectors.any())
-                .build();
-        return docket;
-    }
-
 //    @Bean
-//    public Docket createRestApi() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .apiInfo(apiInfo())
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("com.mocha.controller"))
+//    public Docket addUserDocket() {
+//        Docket docket = new Docket(DocumentationType.SWAGGER_2);
+//        ApiInfo apiInfo = new ApiInfo(
+//                "Spring boot swagger Document API",
+//                "API Document管理",
+//                "V1.0.0",
+//                "",
+//                "1239917467@qq.com",
+//                "",
+//                "");
+//        docket.apiInfo(apiInfo).select()
+//                .apis(RequestHandlerSelectors.basePackage("com.mocha"))
 //                .paths(PathSelectors.any())
 //                .build();
+//        return docket;
 //    }
-//
-//    private ApiInfo apiInfo() {
-//        return new ApiInfoBuilder()
-//                .title("Spring Boot中使用Swagger2构建RESTful APIs")
-//                .description("description")
-//                .termsOfServiceUrl("http://localhost:8080/")
-//                .contact("Leo")
-//                .version("1.0")
-//                .build();
-//    }
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.mocha.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot中使用Swagger2构建RESTful APIs")
+                .description("description")
+                .termsOfServiceUrl("http://localhost:8080/")
+                //.contact("Leo")
+                .version("1.0")
+                .build();
+    }
 
 }
